350463f8a5db36a489f37555e75f9c28fb3e6f73
Vedam Naga Manoj <vedamnagamanoj@gmail.com>
2024-11-20 15:29:19 +0530

    in middle of day 12

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,5 +1,50 @@
 import { useState } from "react";
 
+const Bill = function ({ billAmount, setBillAmount, children }) {
+  return (
+    <div>
+      {children}
+      <input
+        type="text"
+        value={billAmount}
+        onChange={(evnt) => {
+          if (!Number.isFinite(+evnt.target.value)) return;
+          setBillAmount(+evnt.target.value);
+        }}
+        placeholder="Bill Amount"
+        style={{ paddingLeft: "5px" }}
+      />
+      <br />
+    </div>
+  );
+};
+
+const Service = ({ tipPercent, handleTipChange, children }) => {
+  return (
+    <div>
+      {children}
+      <select value={tipPercent} onChange={handleTipChange}>
+        <option value="0">{`Dissatisfied (0%)`}</option>
+        <option value="5">{`It was okay (5%)`}</option>
+        <option value="10">{`It was good (10%)`}</option>
+        <option value="20">{`Absolutely amazing! (20%)`}</option>
+      </select>
+      <br />
+    </div>
+  );
+};
+
+function Total({ billAmount, tipAmount }) {
+  const totalAmount = billAmount + tipAmount;
+  return (
+    <h3>{`You pay $${totalAmount} ($${billAmount} + $${tipAmount} tip)`}</h3>
+  );
+}
+
+function Reset({ handleReset }) {
+  return <button onClick={handleReset}>Reset</button>;
+}
+
 export default function App() {
   const [billAmount, setBillAmount] = useState("");
   const [myPercent, setMyPercent] = useState(0);
@@ -48,49 +93,4 @@ export default function App() {
       )}
     </div>
   );
-}
-
-function Bill({ billAmount, setBillAmount, children }) {
-  return (
-    <div>
-      {children}
-      <input
-        type="text"
-        value={billAmount}
-        onChange={(evnt) => {
-          if (!Number.isFinite(+evnt.target.value)) return;
-          setBillAmount(+evnt.target.value);
-        }}
-        placeholder="Bill Amount"
-        style={{ paddingLeft: "5px" }}
-      />
-      <br />
-    </div>
-  );
-}
-
-function Service({ tipPercent, handleTipChange, children }) {
-  return (
-    <div>
-      {children}
-      <select value={tipPercent} onChange={handleTipChange}>
-        <option value="0">{`Dissatisfied (0%)`}</option>
-        <option value="5">{`It was okay (5%)`}</option>
-        <option value="10">{`It was good (10%)`}</option>
-        <option value="20">{`Absolutely amazing! (20%)`}</option>
-      </select>
-      <br />
-    </div>
-  );
-}
-
-function Total({ billAmount, tipAmount }) {
-  const totalAmount = billAmount + tipAmount;
-  return (
-    <h3>{`You pay $${totalAmount} ($${billAmount} + $${tipAmount} tip)`}</h3>
-  );
-}
-
-function Reset({ handleReset }) {
-  return <button onClick={handleReset}>Reset</button>;
 }
